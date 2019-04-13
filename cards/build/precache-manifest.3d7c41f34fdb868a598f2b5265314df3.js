77ecbf53c643b654d3162950a8feb6e60b475d2e
Kendrick Shen <kshen6@stanford.edu>
2019-04-12 18:04:59 -0700

    lolol

---
+++ cards/build/precache-manifest.3d7c41f34fdb868a598f2b5265314df3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a689fef42d9184dcd31",
+    "url": "/static/js/main.e8597139.chunk.js"
+  },
+  {
+    "revision": "246f946294448963bb77",
+    "url": "/static/js/2.10af5e64.chunk.js"
+  },
+  {
+    "revision": "9a689fef42d9184dcd31",
+    "url": "/static/css/main.461e5429.chunk.css"
+  },
+  {
+    "revision": "8053c930620b79ca8daf96859bd9a87e",
+    "url": "/index.html"
+  }
+];
